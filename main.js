b2751d4b66e11e320e8060a1c7a2ce35bc772809
Renan Abreu <remabreu@hotmail.com>
2020-08-07 12:51:07 -0300

    Carrosel,menu e Inserir Imagens

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-/*---------CARROSEL-----------*/
+/*---------CARROSEL-----FUNÇÃO DE ALTERAR AS IMAGENS E LINKS DE UMA TAG a - PROBLEMAS COM A ADPTAÇÃO DA TELA ------*/
 function banner1(){
     document.getElementById('99').src='img/banner1.jpg';
     
@@ -23,7 +23,7 @@ function banner3(){
     }
     
 
-    /*---------------menu botao-----------------*/
+    /*---------------menu botao------- EVENTO DE COLOCAR UMA ALTURA E LARGURA PROVISORIA----------*/
 
     
     const aparecer = document.getElementById("dh_menu_btn");
@@ -31,10 +31,21 @@ function banner3(){
     
     aparecer.addEventListener("click", () => {
         lista.style.width = "200px";
-        lista.style.height ="200px";
+        lista.style.height ="199px";
         
     })
     
     lista.addEventListener("mouseleave", () => {
         lista.style.width = "0px";
     })
+
+
+    /* ----- ADICIONAR IMAGENS -  ADICIONANDO ATRAVES DE ID EM CADA ELEMENTO DO HTML--*/
+
+    document.getElementById('img1').src = "./img/img1.jpg";
+    
+    document.getElementById('img2').src = "./img/img2.jpg";
+    document.getElementById('img3').src = "./img/img3.jpg";
+    document.getElementById('img4').src = "./img/img4.jpg";
+    document.getElementById('img5').src = "./img/img5.jpg";
+    document.getElementById('img6').src = "./img/img6.jpg";
